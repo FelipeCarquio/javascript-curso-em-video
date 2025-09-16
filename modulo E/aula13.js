539d762e50902e57473d99fee9e0e21f03d8aeee
FelipeCarquio <148922884+FelipeCarquio@users.noreply.github.com>
2025-09-16 10:49:46 -0300

    finalizando o modulo E

---
+++ modulo E/aula13.js
@@ -0,0 +1,18 @@
+//Repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)
+
+
+
+
+/*
+Repetição com teste lógico no inicío
+var c = 1
+while (c <=6 ) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
